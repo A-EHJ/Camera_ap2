36f3d9eab5c65a8d8d2a85b0f6dd9f1962ded5b2
A-EHJ <Abrahamelhage2003@gmail.com>
2024-07-04 09:02:08 -0400

    Agregando libreria para pedir el acceso a la camara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,17 +72,18 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     //  Camerax dependencies
-    implementation (libs.androidx.camera.core)
-    implementation (libs.androidx.camera.camera2)
-    implementation (libs.androidx.camera.lifecycle)
-    implementation (libs.androidx.camera.view)
-    implementation (libs.androidx.camera.extensions)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
 
     //Hilt Dependencies
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
-
+    //permission handling in compose
+    implementation(libs.accompanist.permissions)
 
 }
