488cc880f795bb30ce3562157d90803a3342facb
A-EHJ <Abrahamelhage2003@gmail.com>
2024-07-04 09:42:33 -0400

    Pantallas y viewmodel

--- app/src/main/java/com/example/camera_ap2/MainActivity.kt
+++ app/src/main/java/com/example/camera_ap2/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.camera_ap2.ui.theme.Camera_ap2Theme
+import com.kamatiakash.customcameraappwithjetpackcompose.presentation.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +21,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             Camera_ap2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Box(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        MainScreen()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Camera_ap2Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/camera_ap2/presentation/MainScreen.kt
@@ -0,0 +1,106 @@
+package com.kamatiakash.customcameraappwithjetpackcompose.presentation
+
+import android.Manifest
+import android.os.Build
+import android.widget.Toast
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun MainScreen(
+    viewModel: MainViewModel = hiltViewModel()
+) {
+
+    val permissions = if (Build.VERSION.SDK_INT <= 28) {
+        listOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    } else listOf(Manifest.permission.CAMERA)
+
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = permissions
+    )
+
+    if (!permissionState.allPermissionsGranted) {
+        SideEffect {
+            permissionState.launchMultiplePermissionRequest()
+        }
+    }
+
+
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val configuration = LocalConfiguration.current
+    val screeHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    var previewView: PreviewView
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // we will show camera preview once permission is granted
+        if (permissionState.allPermissionsGranted) {
+            Box(
+                modifier = Modifier
+                    .height(screeHeight * 0.85f)
+                    .width(screenWidth)
+            ) {
+                AndroidView(
+                    factory = {
+                        previewView = PreviewView(it)
+                        viewModel.showCameraPreview(previewView, lifecycleOwner)
+                        previewView
+                    },
+                    modifier = Modifier
+                        .height(screeHeight * 0.85f)
+                        .width(screenWidth)
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .height(screeHeight * 0.15f),
+            contentAlignment = Alignment.Center
+        ) {
+            IconButton(onClick = {
+                if (permissionState.allPermissionsGranted) {
+                    viewModel.captureAndSave(context)
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Please accept permission in app settings",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Outlined.AddCircle,
+                    contentDescription = "Capture Image"
+                )
+            }
+        }
+
+    }
+}
